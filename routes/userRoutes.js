4a38dfd331ea91f72ae5c921a5408df2824aa279
finteger <nashassociatesinc@gmail.com>
2026-03-25 10:44:15 -0600

    Update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 //example of a route parameter
-
 router.get('/users/:id', (req, res) => {
     res.send(`User ID: ${req.params.id}`);
 });
 
+//example of a query parameter
+router.get('/search', (req, res) => {
+    res.send(`Search query: ${req.query.q}`);
+});
+
+//example of a middleware function
+router.use((req, res, next) =>{
+    console.log(`Request URL: ${req.url}.  Time: ${new Date()}`);
+    next();
+});
 
 
 module.exports = router;
