e729d85e463f14f8bcc9f778c57f0a9f3e03fc30
finteger <nashassociatesinc@gmail.com>
2026-04-13 11:11:23 -0600

    Update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,19 +1,43 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
+async function generateToken(req, res) {
+  const secret_key = "super_secret_key";
 
-async function generateToken(req, res){
-    const secret_key = "super_secret_key";
+  const user = {
+    id: 100,
+    date: new Date(),
+  };
 
-    const user = {
-        id: 1,
-        date: new Date(),
-    }
+  const token = jwt.sign(user, secret_key, { expiresIn: "1h" });
 
-    const token = jwt.sign(user, secret_key, { expiresIn: '1h' });
-
-    res.json({ token });
-
+  res.json({ token });
 }
 
-module.exports = { generateToken }
+async function verifyToken(req, res) {
+  const secret_key = "super_secret_key";
+
+  /* 
+       workflow for extracting token from request header
+       some_text auth_token
+       ['some_text', 'auth_token']
+       ['auth_token']
+    */
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) res.status(401).json({ message: "You are not authorized." });
+
+  try {
+    const decoded = jwt.verify(token, secret_key);
+
+    if (decoded) {
+      res.json({ message: "Token is valid.", decoded });
+    } else {
+      res.status(401).json({ message: "Invalid token." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error." });
+  }
+}
+
+module.exports = { generateToken, verifyToken };
